9be1ef087ea36a9386c0bc3e85d83add92538de9
jyyes <jonas.garnero@gmail.com>
2023-11-13 16:18:23 +0100

    shoes

--- src/components/object/Shoes.vue
+++ src/components/object/Shoes.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="w-full cursor-pointer rounded-lg bg-[#EFEFEF] p-2 shadow duration-150 hover:scale-105 hover:shadow-md">
+    <div class="w-full cursor-pointer rounded-lg bg-[#EFEFEF] p-2 shadow duration-300 ease-in-out hover:scale-105 hover:shadow-md">
       <img class="w-full rounded-lg object-cover object-center" src="https://images.unsplash.com/photo-1511556532299-8f662fc26c06?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="product" />
       <p class="my-4 pl-4 font-bold text-gray-500">Categorie</p>
       <p class="mb-4 ml-4 text-xl font-semibold text-gray-800">Product Name</p>
